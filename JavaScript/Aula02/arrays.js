f559fa19df0e20767bcab2feaaa85a3e4e6b27e5
B1nha <binhaup@gmail.com>
2023-06-02 11:02:31 -0300

    Arrays

---
+++ JavaScript/Aula02/arrays.js
@@ -0,0 +1,14 @@
+let minha_var = [];
+
+minha_var.push("Olá");
+minha_var.push(42);
+minha_var.push(70);
+
+for(let i = 0; i < minha_var.length; i++){
+
+    console.log(minha_var[i]);
+
+}
+
+console.log("Somando [1] com [2]");
+console.log(minha_var[1] + minha_var[2]); //[0] - ola //[1] - 42 //[2] - 70
--- JavaScript/Aula02/condicionais.js
+++ JavaScript/Aula02/condicionais.js
@@ -9,11 +9,17 @@ alert("Cuidado com o write!");
 
 let escolha = confirm("Você aceita ser um ganso?");
 
-//esse if(escolha) ta dizendo que é true e o else se for falso
-if(escolha) {
-    
-} else {
 
-}
+//esse if(escolha) ta dizendo que é true e o else se for falso // se for verdadeiro é para printar a imagem, se for falso é para a imagem continuar invisivel
+
+if(escolha) {
+    document.querySelector(".ganso").style.display = "inline-block";
+  // document.querySelector(".ganso").setAttribute("class", "visivel");//atribuindo a classe 
+
+  /*Como manter as classes???
+
+  let minhas_classes = document.querySelector(".ganso").getAttribute("class");
+  document.querySelector(".ganso").setAttribute("class",minhas_classes + " visivel"); */
+} 
 
 prompt("È um pato ou ganso?");
