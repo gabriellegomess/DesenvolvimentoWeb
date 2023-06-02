389c1d757dafc7ad2f2a0513154623da89630b65
B1nha <binhaup@gmail.com>
2023-06-02 11:23:21 -0300

    uPDATES

--- JavaScript/Aula02/arrays.js
+++ JavaScript/Aula02/arrays.js
@@ -1,11 +1,16 @@
 let minha_var = [];
 
+//colando coisa na lista push
 minha_var.push("Olá");
 minha_var.push(42);
 minha_var.push(70);
-
 minha_var[3] = true;
+minha_var.push(30);
 
+
+//tirando coisas da lista pop
+let ultimo = minha_var.pop()
+console.log("Esse é o ultimo: " +ultimo);
 
 for(let i = 0; i < minha_var.length; i++){
 
